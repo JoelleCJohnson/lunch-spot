0729e470648b8c83ff33035e0e20e055953b1e33
Joelle  Johnson <joellej221@gmail.com>
2023-11-08 13:58:37 -0500

    Added basic routing

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 import { Text } from "react-native";
+import { Link } from "expo-router";
 
-export default function Home(){
-    return(
-        <Text style={{ fontSize: 48 }}>Home</Text>
+export default function Home() {
+    return (
+        <>
+            <Text style={{ fontSize: 48 }}>Home</Text>
+            <Link href={"/settings"}>Settings</Link>
+        </>
     )
 }
--- app/settings/index.js
+++ app/settings/index.js
@@ -1,7 +1,11 @@
+import { Link } from "expo-router";
 import { Text } from "react-native";
 
-export default function Settings(){
-    return(
-        <Text>Settings</Text>
+export default function Settings() {
+    return (
+        <>
+            <Text style={{ fontSize: 48 }}>Settings</Text>
+            <Link href="/">Home</Link>
+        </>
     )
 }
